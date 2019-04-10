5699ced2cd3a74efb314b3de8b069027b46fe481
howardbdev <howarddevennish@gmail.com>
2019-04-10 12:15:02 -0400

    add prop - callback for filter button click

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,8 +3,10 @@ import React from 'react'
 class Filters extends React.Component {
 
   render() {
+
     return (
       <div className="ui form">
+
         <h3>Animal type</h3>
         <div className="field">
           <select
@@ -19,7 +21,10 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button
+            className="ui secondary button"
+            onClick={this.props.onFindPetsClick}
+          >Find pets</button>
         </div>
       </div>
     )
