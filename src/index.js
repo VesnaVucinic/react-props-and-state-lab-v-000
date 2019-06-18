76ad9656c5c9f309c25e8ac730537ee3488aef1b
howardbdev <howarddevennish@gmail.com>
2019-06-18 08:24:27 -0400

    set up redux store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './fetch-setup';
+import {
+  createStore,
+  combineReducers,
+  compose
+} from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1. Get the ingredients to build a store:
+//  a reducer, ?? middleware if needed ??
+
+const initialState = {
+  type: "all"
+}
+
+const filters = (state = initialState, action) => {
+  switch (action.type) {
+    case "CHANGE_FILTER":
+      return {
+        type: action.newFilter
+      }
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  filters
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancer()
+);
+
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
